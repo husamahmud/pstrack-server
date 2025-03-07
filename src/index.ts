cb4e94391c56e505c6cb63a0376c6d5f9d4fe743
husamahmud <husamahmud@gmail.com>
2025-03-07 03:53:39 +0300

    init project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import type { Request, Response } from "express";
+import express from "express";
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to the Express + TypeScript Server!" });
+});
+
+app.listen(port, () => {
+  console.log(`\x1b[47mhttp://localhost:%s\x1b[0m`, port);
+});

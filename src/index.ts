ce6e25a4c340c5a3a6a5f0f1dd373af3b19ba1f4
husamahmud <husamahmud@gmail.com>
2025-03-07 04:19:31 +0300

    add biome

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import type { Request, Response } from "express";
-import express from "express";
+import type { Request, Response } from 'express'
+import express from 'express'
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT;
+const port = process.env.PORT
 
-app.get("/", (res: Response) => {
-  res.json({ message: "Welcome to the Express + TypeScript Server!" });
-});
+app.get('/', (res: Response) => {
+  res.json({ message: 'Welcome to the Express + TypeScript Server!' })
+})
 
 app.listen(port, () => {
-  console.log(`\x1b[47mhttp://localhost:%s\x1b[0m`, port);
-});
+  console.log('Server started')
+})
